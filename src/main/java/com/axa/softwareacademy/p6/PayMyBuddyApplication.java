eadfc08168e6f08558e60ab381d5fb383a1a6c47
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-04-29 12:08:44 +0200

    Init commit - Configure Git Ignore - Spring Spring Boot and Hibernate dependencies

---
+++ src/main/java/com/axa/softwareacademy/p6/PayMyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.axa.softwareacademy.p6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMyBuddyApplication.class, args);
+	}
+
+}
