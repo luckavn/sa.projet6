b2f0f3759c0ecfe875f1218f4676c37ba9ee5cfe
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-04-30 16:34:13 +0200

    Configure and implement run test profile for H2 database to launch (MySQL for prod)

---
+++ src/main/java/com/axa/softwareacademy/p6/configuration/H2TestProfileJPAConfig.java
@@ -0,0 +1,36 @@
+package com.axa.softwareacademy.p6.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = {
+        "com.axa.softwareacademy.p6.dao",
+})
+
+@EnableTransactionManagement
+public class H2TestProfileJPAConfig {
+
+    @Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("sa");
+
+        return dataSource;
+    }
+
+    // configure entityManagerFactory
+    // configure transactionManager
+    // configure additional Hibernate properties
+}
+
--- src/test/java/com/axa/softwareacademy/p6/PayMyBuddyApplicationTests.java
+++ src/test/java/com/axa/softwareacademy/p6/PayMyBuddyApplicationTests.java
@@ -1,13 +1,18 @@
 package com.axa.softwareacademy.p6;
 
+import com.axa.softwareacademy.p6.configuration.H2TestProfileJPAConfig;
+import javafx.application.Application;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = {PayMyBuddyApplication.class, H2TestProfileJPAConfig.class})
+@ActiveProfiles("test")
 class PayMyBuddyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
