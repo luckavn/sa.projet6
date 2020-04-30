4a150b2fa251ff3faf6041fd02ef97b4f84777e8
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-04-30 15:37:43 +0200

    Implementing JpaConfig to handle databases types

--- src/main/java/com/axa/softwareacademy/p6/configuration/JpaConfig.java
+++ src/main/java/com/axa/softwareacademy/p6/configuration/JpaConfig.java
@@ -1,4 +1,21 @@
 package com.axa.softwareacademy.p6.configuration;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
 public class JpaConfig {
+    @Bean(name = "mySqlDataSource")
+    @Primary
+    public DataSource mySqlDataSource()
+    {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306/pmb");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("rootroot");
+        return dataSourceBuilder.build();
+    }
 }
+
