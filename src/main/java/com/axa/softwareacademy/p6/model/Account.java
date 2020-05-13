af32c3a7f2ba49ce6ec0a34ff693e691db4bf28a
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-05-13 10:17:19 +0200

    (feature)Revising Account Model for database

--- src/main/java/com/axa/softwareacademy/p6/model/Account.java
+++ src/main/java/com/axa/softwareacademy/p6/model/Account.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
@@ -11,9 +12,33 @@ import javax.persistence.*;
 @Table(name = "account")
 public class Account {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
     int id;
     @OneToOne
     User user;
     float balance;
     String currency;
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) { this.user = user; }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) { this.balance = balance; }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) { this.currency = currency; }
 }
