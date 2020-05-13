f59a49e9fd3b9eac463d141c14768ecda1bc0701
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-05-13 10:21:07 +0200

    (technical)revising schema.sql for database init

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,14 @@
-DROP TABLE IF EXISTS user;
 CREATE TABLE user
 (
     id        INT          NOT NULL AUTO_INCREMENT,
     firstName VARCHAR(100) NOT NULL,
     lastName  VARCHAR(100) NOT NULL,
-    email     VARCHAR(100) NOT NULL,
+    password  VARCHAR(300) NOT NULL AUTO_INCREMENT,
+    email     VARCHAR(100) NOT NULL UNIQUE,
     account   INT          NOT NULL,
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS account;
 CREATE TABLE account
 (
     id       INT         NOT NULL AUTO_INCREMENT,
@@ -19,7 +18,6 @@ CREATE TABLE account
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS bank_account;
 CREATE TABLE bank_account
 (
     id    INT         NOT NULL AUTO_INCREMENT,
@@ -30,7 +28,6 @@ CREATE TABLE bank_account
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS credit_card;
 CREATE TABLE credit_card
 (
     id             INT         NOT NULL AUTO_INCREMENT,
@@ -41,7 +38,16 @@ CREATE TABLE credit_card
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS app_revenue;
+CREATE TABLE refill
+(
+    id         INT         NOT NULL AUTO_INCREMENT,
+    creditCard INT         NOT NULL,
+    account    INT         NOT NULL,
+    sum        FLOAT       NOT NULL,
+    status     VARCHAR(10) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE app_revenue
 (
     id           INT   NOT NULL AUTO_INCREMENT,
@@ -49,7 +55,6 @@ CREATE TABLE app_revenue
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS commission;
 CREATE TABLE commission
 (
     id               INT        NOT NULL AUTO_INCREMENT,
@@ -59,7 +64,6 @@ CREATE TABLE commission
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS payment;
 CREATE TABLE payment
 (
     id                 INT         NOT NULL AUTO_INCREMENT,
@@ -73,18 +77,19 @@ CREATE TABLE payment
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS transfer;
+
 CREATE TABLE transfer
 (
-    id                 INT         NOT NULL AUTO_INCREMENT,
-    user INT         NOT NULL,
-    account   INT         NOT NULL,
-    bankAccount                INT       NOT NULL,
-    sum              FLOAT NOT NULL,
-    status               VARCHAR(15) NOT NULL,
-    type        VARCHAR(15)        NOT NULL,
+    id          INT         NOT NULL AUTO_INCREMENT,
+    user        INT         NOT NULL,
+    account     INT         NOT NULL,
+    bankAccount INT         NOT NULL,
+    sum         FLOAT       NOT NULL,
+    status      VARCHAR(15) NOT NULL,
+    type        VARCHAR(15) NOT NULL,
     PRIMARY KEY (id)
 );
+
 
 
 
