cc1f27082d34b414313cb879d0c91092001f77dd
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-05-13 10:21:35 +0200

    (feature)Revising Main App and Properties

--- src/main/java/com/axa/softwareacademy/p6/PayMyBuddyApplication.java
+++ src/main/java/com/axa/softwareacademy/p6/PayMyBuddyApplication.java
@@ -5,11 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.axa.softwareacademy.p6.dao", "com.axa.softwareacademy.p6.rest", "com.axa.softwareacademy.p6.service", "com.axa.softwareacademy.p6.repository"})
 public class PayMyBuddyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayMyBuddyApplication.class, args);
 	}
-
 }
