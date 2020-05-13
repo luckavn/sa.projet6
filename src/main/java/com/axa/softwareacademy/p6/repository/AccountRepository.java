cec4fe5e37c26be0300f06844c1b8177c7d89f36
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-05-13 10:17:44 +0200

    (feature)Creating Account Repository

--- src/main/java/com/axa/softwareacademy/p6/repository/AccountRepository.java
+++ src/main/java/com/axa/softwareacademy/p6/repository/AccountRepository.java
@@ -1,4 +1,14 @@
 package com.axa.softwareacademy.p6.repository;
 
-public interface AccountRepository {
+import com.axa.softwareacademy.p6.model.Account;
+import com.axa.softwareacademy.p6.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    Account findById(int accountId);
+
+    Account findByUser(User user);
 }
