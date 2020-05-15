41d4f1f9074030699d2e4987048db95370fc02c7
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-05-15 10:12:35 +0200

    (tests)revising app launch test

--- src/test/java/com/axa/softwareacademy/p6/PayMyBuddyApplicationTests.java
+++ src/test/java/com/axa/softwareacademy/p6/PayMyBuddyApplicationTests.java
@@ -1,18 +1,12 @@
 package com.axa.softwareacademy.p6;
 
-import com.axa.softwareacademy.p6.configuration.H2TestProfileJPAConfig;
-import javafx.application.Application;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = {PayMyBuddyApplication.class, H2TestProfileJPAConfig.class})
-@ActiveProfiles("test")
+@SpringBootTest
 class PayMyBuddyApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
