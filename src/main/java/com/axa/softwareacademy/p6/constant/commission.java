382ec3ae87ee77c1c19fc283496527995860411f
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-05-27 17:18:01 +0200

    (bugfix)externalize commission pourcentages to constant class

--- src/main/java/com/axa/softwareacademy/p6/constant/commission.java
+++ src/main/java/com/axa/softwareacademy/p6/constant/commission.java
@@ -1,4 +1,7 @@
 package com.axa.softwareacademy.p6.constant;
 
 public class commission {
+    public static final double fourPourcentCommission = 0.04;
+    public static final double fivePourcentCommission = 0.05;
+    public static final double sixPourcentCommission = 0.06;
 }
