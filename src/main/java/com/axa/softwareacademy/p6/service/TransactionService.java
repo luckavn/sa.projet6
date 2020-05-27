57db3825032c305b969d306a13f87c5f38043e2c
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-05-27 17:20:54 +0200

    (bugfix)adding some comments and info to transaction service methods

--- src/main/java/com/axa/softwareacademy/p6/service/TransactionService.java
+++ src/main/java/com/axa/softwareacademy/p6/service/TransactionService.java
@@ -14,17 +14,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Date;
 
 @Service
 @Transactional
 public class TransactionService {
     private static final Logger logger = LogManager.getLogger(TransactionService.class);
-    @Autowired
-    Refill refill;
-    @Autowired
-    Payment payment;
-    @Autowired
-    Transfer transfer;
     @Autowired
     UserRepository userRepository;
     @Autowired
@@ -34,9 +29,17 @@ public class TransactionService {
     @Autowired
     TransferRepository transferRepository;
 
+    /**
+     * @param id of user that will do a refill to his Pay my buddy account thanks to this method
+     * @param sum is the amount of the refill
+     * @return the refill added to user's account
+     * @throws Exception if the user doesn't exists
+     */
     public Refill createRefillAndAddToUserAccount(int id, float sum) throws Exception {
         User userConcerned = userRepository.findById(id);
-        Refill newRefill = refill.createRefill(userConcerned.getCreditCard(), userConcerned.getAccount(), sum);
+        Refill newRefill = new Refill(sum);
+        newRefill.setCreditCard(userConcerned.getCreditCard());
+        newRefill.setAccount(userConcerned.getAccount());
 
         if(userRepository.existsById(id)) {
             refillRepository.save(newRefill);
@@ -53,11 +56,34 @@ public class TransactionService {
         return newRefill;
     }
 
+    /**
+     * @param id of user that will do a payment to a friend
+     * @param friendId of user that will receive the payment
+     * @param sum is the amount of the payment that will be decrease of 5% commission
+     * @return the payment executed
+     * @throws Exception if the transmitter's account balance is not high enough to perform the payment
+     */
     public Payment createPaymentAndUpdateBalances(int id, int friendId, float sum) throws Exception {
+        //Identify users
+        Date date = new Date();
         User userThatWillBeCharged = userRepository.findById(id);
         User userThatWillBeFill = userRepository.findById(friendId);
-        Payment newPayment = payment.createPayment(userThatWillBeCharged.getAccount(), userThatWillBeFill.getAccount(), sum);
 
+        //Initialize payment
+        Payment newPayment = new Payment(sum);
+        newPayment.setAccountTransmitter(userThatWillBeCharged.getAccount());
+        newPayment.setAccountRecipient(userThatWillBeFill.getAccount());
+
+        //Calculate commission and apply
+        float commission = Payment.calculatePaymentCommission(sum);
+        newPayment.setCommissionAmount(commission);
+
+        //Recalculate sum of payment
+        sum = sum - commission;
+        newPayment.setSum(sum);
+        newPayment.setPaymentDate(date);
+
+        //Do treatment
         if (userThatWillBeCharged.getAccount().getBalance() > newPayment.getSum()) {
             paymentRepository.save(newPayment);
             logger.info("New payment saved and linked to users");
@@ -77,10 +103,22 @@ public class TransactionService {
         return newPayment;
     }
 
+    /**
+     * @param id of user that will do a transfer to his bank account from his Pay My Buddy account
+     * @param sum is the amount of the transfer
+     * @return the transfer
+     * @throws Exception if the user's account balance is not high enough to perform the transfer
+     */
     public Transfer createTransferAndUpdateUserAccount(int id, float sum) throws Exception {
+        //Identify users
         User userConcerned = userRepository.findById(id);
-        Transfer newTransfer = transfer.createTransfer(userConcerned.getAccount(), userConcerned.getBankAccount(), sum);
 
+        //Initialize transfer
+        Transfer newTransfer = new Transfer(sum);
+        newTransfer.setAccount(userConcerned.getAccount());
+        newTransfer.setBankAccount(userConcerned.getBankAccount());
+
+        //Do treatment
         if (userConcerned.getAccount().getBalance() > sum) {
             newTransfer.setUser(userConcerned);
             transferRepository.save(newTransfer);
