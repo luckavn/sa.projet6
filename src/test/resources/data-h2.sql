25b80359cd16908d716f68bb712274860b449d50
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-05-27 17:23:54 +0200

    (tests)update some resources and data for test context

--- src/test/resources/data-h2.sql
+++ src/test/resources/data-h2.sql
@@ -1,4 +1,5 @@
 INSERT INTO ACCOUNT(id, balance, currency) values(1, 500, 'euro');
 INSERT INTO ACCOUNT(id, balance, currency) values(2, 500, 'euro');
+INSERT INTO CREDIT_CARD(id, card_number, expiration_date, cvv_number) values(3, '234567898765435678', '12/25', '123');
 INSERT INTO USER(id, first_name, last_name, email, password, account_id) values(1, 'John', 'Doe', 'john.doe@gmail.com', 'password', 1);
-INSERT INTO USER(id, first_name, last_name, email, password, account_id) values(2, 'Marc', 'Doe', 'marc.doe@gmail.com', 'password', 2);
+INSERT INTO USER(id, first_name, last_name, email, password, account_id, credit_card_id) values(2, 'Marc', 'Doe', 'marc.doe@gmail.com', 'password', 2, 3);
