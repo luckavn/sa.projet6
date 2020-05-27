f42f5e295f245c2fefd5d064d9796061bba29506
VANNIER Lucas <S643362@siege.axa-fr.intraxa>
2020-05-27 17:22:17 +0200

    (bugfix)revising User pojo in order to allow hibernate cascade saving

--- src/main/java/com/axa/softwareacademy/p6/model/User.java
+++ src/main/java/com/axa/softwareacademy/p6/model/User.java
@@ -2,15 +2,12 @@ package com.axa.softwareacademy.p6.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.UUID;
 
-@Component
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
@@ -29,12 +26,27 @@ public class User {
     private String password;
     @OneToMany
     private List<User> friends;
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "account_id", referencedColumnName="id")
     private Account account;
     @OneToOne
     private BankAccount bankAccount;
     @OneToOne
     private CreditCard creditCard;
+
+    public User() { }
+
+    public User(int id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String firstName, String lastName, String email, String password) {
+        this(0, firstName, lastName, email, password);
+    }
 
     public Integer getId() {
         return id;
@@ -46,68 +58,48 @@ public class User {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public void setEmail(String email) { this.email = email; }
 
     public List<User> getFriends() {
         return friends;
     }
 
-    public void setFriends(List<User> friends) {
-        this.friends = friends;
-    }
+    public void setFriends(List<User> friends) { this.friends = friends; }
 
     public Account getAccount() {
         return account;
     }
 
-    public void setAccount(Account account) {
-        this.account = account;
-    }
+    public void setAccount(Account account) { this.account = account; }
 
     public BankAccount getBankAccount() {
         return bankAccount;
     }
 
-    public void setBankAccount(BankAccount bankAccount) {
-        this.bankAccount = bankAccount;
-    }
+    public void setBankAccount(BankAccount bankAccount) { this.bankAccount = bankAccount; }
 
     public CreditCard getCreditCard() {
         return creditCard;
     }
 
-    public void setCreditCard(CreditCard creditCard) {
-        this.creditCard = creditCard;
-    }
+    public void setCreditCard(CreditCard creditCard) { this.creditCard = creditCard; }
 
-    public String getPassword() { return password; }
+    public String getPassword() {
+        return password;
+    }
 
     public void setPassword(String password) { this.password = password; }
 
-    public User createUser(String firstName, String lastName, String email, String password) {
-        User newUser =  new User();
-        newUser.setFirstName(firstName);
-        newUser.setLastName(lastName);
-        newUser.setEmail(email);
-        newUser.setPassword(password);
-        return newUser;
-    }
 }
